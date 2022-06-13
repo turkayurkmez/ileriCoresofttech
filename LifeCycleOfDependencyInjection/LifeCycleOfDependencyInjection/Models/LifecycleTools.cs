2a85cbf7aac12a30c49ef8b214775a75834afe7f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-13 14:30:50 +0300

    creating guid value when calling constructors (creating instance)

---
+++ LifeCycleOfDependencyInjection/LifeCycleOfDependencyInjection/Models/LifecycleTools.cs
@@ -0,0 +1,56 @@
+﻿namespace LifeCycleOfDependencyInjection.Models
+{
+    public interface IGuidGenerator
+    {
+        Guid GuidValue { get; set; }        
+    }
+
+    public interface IScopedGuidGenerator: IGuidGenerator
+    {
+            
+    }
+
+    public interface ISingletonGuidGenerator : IGuidGenerator
+    {
+
+    }
+
+    public interface ITransientGuidGenerator : IGuidGenerator
+    {
+
+    }
+
+    public class ScopeguidGenerator : IScopedGuidGenerator
+    {
+        public Guid GuidValue { get; set ; }
+        public ScopeguidGenerator()
+        {
+            GuidValue = Guid.NewGuid();
+        }
+    }
+
+    public class SingletonGuidGenerator : ISingletonGuidGenerator
+    {
+        public Guid GuidValue { get; set; }
+        public SingletonGuidGenerator()
+        {
+            GuidValue = Guid.NewGuid();
+        }
+        
+    }
+
+    public class TransientGuidGenerator : ITransientGuidGenerator
+    {
+        public Guid GuidValue { get ; set ; }
+        public TransientGuidGenerator()
+        {
+            GuidValue = Guid.NewGuid();
+        }
+    }
+
+
+
+
+
+
+}
